aaa9d31b20d6741a19da8fdad43e622a3dea53b7
Jhanvi Mahawar <jhanvi21war@gmail.com>
2025-07-06 23:46:25 +0530

    Pushing existing project to existing GitHub repo

---
+++ RetailInventory - Lab3/RetailInventory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RetailInventory.Models;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new retailDb())
+            {
+       
+                context.Database.EnsureCreated();
+
+                if (!context.Categories.Any())
+                {
+                    var electronics = new Category { Name = "Electronics" };
+                    context.Categories.Add(electronics);
+
+                    context.Products.Add(new Product
+                    {
+                        Name = "Laptop",
+                        Stock = 10,
+                        Category = electronics
+                    });
+
+                    context.SaveChanges();
+                }
+
+                Console.WriteLine("Database created and data inserted.");
+            }
+        }
+    }
+}
---
+++ RetailInventory/RetailInventory/retailDb.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailInventory
+{
+    internal class retailDb : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=RetailInventoryDb;Trusted_Connection=True;");
+        }
+    }
+}
